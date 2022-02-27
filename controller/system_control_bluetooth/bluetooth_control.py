70d448bfef84417294427e1c56713d32cf76a84e
rohanku <rohankumar@berkeley.edu>
2022-02-26 20:15:46 -0800

    add controller for servos and telemetry, need to figure out why winding motor is not working

--- controller/system_control_bluetooth/bluetooth_control.py
+++ controller/system_control_bluetooth/bluetooth_control.py
@@ -1,15 +1,12 @@
 #!/usr/bin/env python3
 
-# import bluetooth
-# import pygame, time, sys
-# from pygame.locals import *
+import bluetooth
+import pygame, time, sys
+from pygame.locals import *
 
 import subprocess
 import asyncio
-import datetime as dt
-
 import serial_asyncio
-import aioconsole
 
 DEVICE_MAC_ADDRESS = '40-f5-20-45-22-d2'
 CONNECTION_TIMEOUT = 2
@@ -17,7 +14,7 @@ CONNECTION_TIMEOUT = 2
 ANGLE_MIN = 0
 ANGLE_MAX = 180
 angle = 0
-angle_change_rate = 60
+angle_increment = 20
 
 sent_angle = 0
 prev_t = 0
@@ -32,48 +29,28 @@ def run_terminal_command(args, timeout_fail=True):
 
     return p.returncode
 
-def run_controller():
+async def run_controller():
+    reader, writer = await serial_asyncio.open_serial_connection(url=port, baudrate=baudrate)
+    writer.write(b"Controller connected.\n")
+
     pygame.init()
     screen = pygame.display.set_mode((640, 480))
     pygame.display.set_caption('Packagebot Controller')
     pygame.mouse.set_visible(0)
 
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect((HOST, PORT))
-        while True:
-            t = pygame.time.get_ticks();
-            delta_t = pygame.time.Clock().tick(30);
-            keys = pygame.key.get_pressed()
-            if t - prev_t > 20:
-                print(angle)
-                send_angle = int(angle)
-                if send_angle != sent_angle:
-                    s.sendall((str(send_angle)+"\n").encode('utf-8'))
-                    sent_angle = send_angle
-                prev_t = t
-            if keys[K_UP]:
-                angle = min(ANGLE_MAX, angle + delta_t * angle_change_rate / 1000)
-            if keys[K_DOWN]:
-                angle = max(ANGLE_MIN, angle - delta_t * angle_change_rate / 1000)
-            for event in pygame.event.get():
-                if event.type == QUIT:
-                    pygame.quit()
-                    sys.exit()
-
-async def receive(reader):
     while True:
         data = await reader.readuntil(b'\n')
         print(f'(recv): {data.strip().decode()}')
-
-async def send(writer):
-    writer.write(b"Connection initialized.\n")
-    stdin, _ = await aioconsole.get_standard_streams()
-    async for line in stdin:
-        data = line.strip()
-        if not data:
-            continue
-        writer.write(line)
-
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    angle = min(ANGLE_MAX, angle + angle_increment)
+                if event.key == pygame.K_DOWN:
+                    angle = max(ANGLE_MIN, angle - angle-increment)
+                print(angle)
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
 
 async def open_bluetooth_terminal(port, baudrate):
     print("Setting up device at %s..." % DEVICE_MAC_ADDRESS)
@@ -87,10 +64,7 @@ async def open_bluetooth_terminal(port, baudrate):
         print("Failed to connect to device. Exiting...")
         return
     print("Connected successfully.")
-    reader, writer = await serial_asyncio.open_serial_connection(url=port, baudrate=baudrate)
-    receiver = receive(reader)
-    sender = send(writer)
-    await asyncio.wait([receiver, sender])
+    await asyncio.wait(run_controller())
 
 loop = asyncio.get_event_loop()
 task = open_bluetooth_terminal('/dev/cu.ESP32', 115200)
--- controller/system_control_bluetooth/system_control_bluetooth.ino
+++ controller/system_control_bluetooth/system_control_bluetooth.ino
@@ -1,19 +1,36 @@
-#include "WiFi.h"
+#include "SparkFunLSM6DSO.h"
+#include "Wire.h"
+#include "BluetoothSerial.h"
+#include "ArduinoJson.h"
 
-#define SERVO_1 25
+// IMU constants
+#define MEASUREMENT_INTERVAL 2000
+
+// Servo constants
+#define NUM_SERVOS 2
+const int servo_pin[] = {26, 25};
+const bool servo_clockwise[] = {true, false};
 #define PULSE_MIN 500
 #define PULSE_MAX 2500
 #define PULSE_PERIOD 20000
 #define RANGE 270
-#define DELIMITER ' '
 
-//This example code is in the Public Domain (or CC0 licensed, at your option.)
-//By Evandro Copercini - 2018
-//
-//This example creates a bridge between Serial and Classical Bluetooth (SPP)
-//and also demonstrate that SerialBT have the same functionalities of a normal Serial
+// Motor constants
+#define WINDING_MOTOR_1 13
+#define WINDING_MOTOR_2 12
+#define WINDING_MOTOR_ENABLE 27
+#define WINDING_MOTOR_PWM 0
+#define PWM_FREQ 30000
+#define PWM_RES 8
+#define DUTY_CYCLE_MAX 255
 
-#include "BluetoothSerial.h"
+// Serialization constants
+#define DELIMITER '\n'
+
+LSM6DSO IMU;
+bool IMU_setup_success;
+unsigned long IMU_last_measurement = 0;
+
 
 #if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
 #error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
@@ -21,72 +38,210 @@
 
 BluetoothSerial SerialBT;
 
-/*void setup() {
-  Serial.begin(115200);
-  SerialBT.begin("ESP32test"); //Bluetooth device name
-  Serial.println("The device started, now you can pair it with bluetooth!");
-}
-
-void loop() {
-  if (Serial.available()) {
-    SerialBT.write(Serial.read());
-  }
-  if (SerialBT.available()) {
-    Serial.write(SerialBT.read());
-  }
-  delay(20);
-}*/
-
-unsigned long pulseStart = 0;
-unsigned long pulseLength;
+unsigned long servo_pulse_start[] = {0, 0};
+unsigned long servo_pulse_length[] = {0, 0};
 
 String command = "";
 unsigned long command_index = 0;
 
+/* OPCODES
+ *  0 - Turn servo
+ *  1 - Set winding motor
+ */
+
+/* TELEMETRY TYPES
+ *  0 - IMU data
+ */
+
 void processReceivedValue(char b){
   if(b == DELIMITER){
-    int angle = command.toInt();
-    String debug = String("Turning servo: ") + angle;
-    Serial.println(debug);
-    turnServo(SERVO_1, angle);
+    Serial.println(command);
+    DynamicJsonDocument doc(1024);
+    deserializeJson(doc, command);
+    int opcode = doc["command"];
+    JsonArray arguments = doc["args"];
+    if (arguments != NULL) {
+      switch (opcode) {
+        case 0:
+          turnServo(arguments);
+          break;
+        case 1:
+          setWindingMotor(arguments);
+          break;
+      }
+    } else {
+      Serial.println("Opcode or arguments not passed in");
+    }
     command = "";
   }
-  else if('0' <= b && b <= '9'){
+  else {
     command.concat(b);
-  } else {
-    command = "";
   }
  
   return;
 }
 
-void turnServo(int pin, int angle) {
-  unsigned long curr_t = micros();
-  if (pulseStart + PULSE_PERIOD > curr_t) return;
-  pulseStart = curr_t;
-  pulseLength = PULSE_MIN + (PULSE_MAX - PULSE_MIN) * angle/RANGE;
-  digitalWrite(pin, HIGH);
+bool setupIMU() {
+  bool success = IMU.begin();
+  if (!IMU.initialize(BASIC_SETTINGS)) {
+    Serial.println("Failed to load IMU settings");
+  }
+  return success;
 }
 
-void checkServo(int pin) {
-  if (micros() > pulseStart + pulseLength) {
-    digitalWrite(pin, LOW);
+void sendIMUData() {
+  unsigned long curr_t = millis();
+  if (IMU_last_measurement + MEASUREMENT_INTERVAL > curr_t) return;
+  Serial.print("\nAccelerometer:\n");
+  Serial.print(" X = ");
+  Serial.println(IMU.readFloatAccelX(), 3);
+  Serial.print(" Y = ");
+  Serial.println(IMU.readFloatAccelY(), 3);
+  Serial.print(" Z = ");
+  Serial.println(IMU.readFloatAccelZ(), 3);
+
+  Serial.print("\nGyroscope:\n");
+  Serial.print(" X = ");
+  Serial.println(IMU.readFloatGyroX(), 3);
+  Serial.print(" Y = ");
+  Serial.println(IMU.readFloatGyroY(), 3);
+  Serial.print(" Z = ");
+  Serial.println(IMU.readFloatGyroZ(), 3);
+
+  Serial.print("\nThermometer:\n");
+  Serial.print(" Degrees F = ");
+  Serial.println(IMU.readTempF(), 3);
+  DynamicJsonDocument doc(1024);
+  doc["type"] = 0;
+  doc["data"]["a_x"] = IMU.readFloatAccelX();
+  doc["data"]["a_y"] = IMU.readFloatAccelY();
+  doc["data"]["a_z"] = IMU.readFloatAccelZ();
+  
+  doc["data"]["g_x"] = IMU.readFloatGyroX();
+  doc["data"]["g_y"] = IMU.readFloatGyroY();
+  doc["data"]["g_z"] = IMU.readFloatGyroZ();
+  
+  doc["data"]["temp"] = IMU.readTempF();
+
+  serializeJson(doc, Serial);
+  Serial.println();
+  serializeJson(doc, SerialBT);
+  SerialBT.println();
+
+  IMU_last_measurement = curr_t;
+}
+
+void setupServos() {
+  for (int i = 0; i < NUM_SERVOS; i++) {
+    pinMode(servo_pin[i], OUTPUT);
+    digitalWrite(servo_pin[i], LOW);
+  }
+}
+
+void turnServo(JsonArray arguments) {
+  if (arguments.size() != 2) {
+    Serial.println("Incorrect number of arguments for turning servos");
+    return;
+  }
+  int id = arguments[0];
+  if (id < 0 || id >= NUM_SERVOS) {
+    Serial.println("Invalid servo id");
+  }
+  int desired_angle = arguments[1];
+  if (desired_angle < 0 || desired_angle >= RANGE) {
+    Serial.println("Invalid servo angle");
+  }
+  int true_angle = servo_clockwise[id] ? desired_angle : 270 - desired_angle;
+  char buffer[40];
+  sprintf(buffer, "Turning servo %d: %d.", id, true_angle);
+  Serial.println(buffer);
+  unsigned long curr_t = micros();
+  if (servo_pulse_start[id] + PULSE_PERIOD > curr_t) return;
+  servo_pulse_start[id] = curr_t;
+  servo_pulse_length[id] = PULSE_MIN + (PULSE_MAX - PULSE_MIN) * true_angle/RANGE;
+  digitalWrite(servo_pin[id], HIGH);
+}
+
+void checkServos() {
+  for (int i = 0; i < NUM_SERVOS; i++) {
+    if (micros() > servo_pulse_start[i] + servo_pulse_length[i]) {
+      digitalWrite(servo_pin[i], LOW);
+    }
+  }
+}
+
+void setupWindingMotor() {
+  pinMode(WINDING_MOTOR_1, OUTPUT);
+  pinMode(WINDING_MOTOR_2, OUTPUT);
+  pinMode(WINDING_MOTOR_ENABLE, OUTPUT);
+  
+  // Set up PWM channel
+  ledcSetup(WINDING_MOTOR_PWM, PWM_FREQ, PWM_RES);
+  
+  // Attach PWM channel to GPIO
+  ledcAttachPin(WINDING_MOTOR_ENABLE, WINDING_MOTOR_PWM);
+}
+
+void setWindingMotor(JsonArray arguments) {
+  if (arguments.size() != 2) {
+    Serial.println("Incorrect number of arguments for turning servos");
+    return;
+  }
+  int dir = arguments[0]; // 0 - Forward, 1 - Backward, 2 - Stop
+  if (dir < 0 || dir > 2) {
+    Serial.println("Invalid motor state");
+    return;
+  }
+  int duty_cycle = arguments[1];
+  if (dir != 2 && (duty_cycle < 0 || duty_cycle > DUTY_CYCLE_MAX)) {
+    Serial.println("Invalid duty cycle");
+    return;
+  }
+
+  char buffer[70];
+  sprintf(buffer, "Setting winding motor to state %d, duty cycle %d.", dir, duty_cycle);
+  Serial.println(buffer);
+  switch (dir) {
+    case 0:
+      digitalWrite(WINDING_MOTOR_1, HIGH);
+      digitalWrite(WINDING_MOTOR_2, LOW);
+      ledcWrite(WINDING_MOTOR_PWM, duty_cycle);   
+    case 1:
+      digitalWrite(WINDING_MOTOR_1, LOW);
+      digitalWrite(WINDING_MOTOR_2, HIGH);
+      ledcWrite(WINDING_MOTOR_PWM, duty_cycle); 
+    case 2:
+      digitalWrite(WINDING_MOTOR_1, LOW);
+      digitalWrite(WINDING_MOTOR_2, LOW);
   }
 }
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(115200);
   SerialBT.begin("ESP32");
-  pinMode(SERVO_1, OUTPUT);
-  digitalWrite(SERVO_1, LOW);
+  delay(500); 
+  
+  Wire.begin(23, 22);
+  delay(10);
+
+  // Set up IMU
+  IMU_setup_success = setupIMU();
+
+  // Set up servos
+  setupServos();
+
+  // Set up winding motor
+  setupWindingMotor();
+  
 }
 
 void loop() {
-  checkServo(SERVO_1);
+  checkServos();
+  if (IMU_setup_success)
+    sendIMUData();
   if (SerialBT.available()) {
     char c = SerialBT.read();
+    Serial.print(c);
     processReceivedValue(c);
-    Serial.write(c);
   }
 }
